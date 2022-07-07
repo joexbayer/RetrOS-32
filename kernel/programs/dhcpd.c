51c025bef41d5d03d350b7eb5ea0f711a44a4926
Joe Bayer <jb71999@gmail.com>
2022-07-07 09:27:16 +0200

    DHCP boot request.

--- kernel/programs/dhcpd.c
+++ kernel/programs/dhcpd.c
@@ -3,19 +3,36 @@
 #include <net/socket.h>
 #include <net/ipv4.h>
 #include <terminal.h>
+#include <memory.h>
 #include <util.h>
 
 static socket_t dhcp_socket;
 static struct dhcp_state dhcp_state;
 
 
-static int dhcp_add_option(struct dhcp* dhcp, int offset, uint8_t opcode, uint8_t opsz, uint32_t val)
+static int dhcp_add_option(struct dhcp* dhcp, int offset, uint8_t opcode, uint8_t opsz, uint8_t* val)
 {
     if(offset >= 128)
         return 0;
 
     uint8_t* ptr = (uint8_t*) (&dhcp->dhcp_options)+offset;
+    ptr += sizeof(struct dhcp);
+    
+    if(255 == opcode){
+        *ptr = 255;
+        return 1;
+    }
 
+    *ptr = opcode;
+    ptr++;
+
+    *ptr = opsz;
+    ptr++;  
+
+    for (int i = 0; i < opsz; i ++)
+        ptr[i] = val[i];
+
+    return opsz + 2;
 }
 
 void dhcpd()
@@ -23,13 +40,22 @@ void dhcpd()
     dhcp_socket = socket(AF_INET, SOCK_DGRAM, 0);
     dhcp_state.state = DHCP_PENDING;
 
+    int optoff = 0;
+    int opt1 = 1;
+    int opt0 = 0;
+    struct dhcp* dhcp_disc = alloc(sizeof(struct dhcp));
+    DHCP_DISCOVERY(dhcp_disc);
+
+    optoff += dhcp_add_option(dhcp_disc, optoff,  53, 1, (uint8_t *) &opt1);
+    optoff += dhcp_add_option(dhcp_disc, optoff, 255, 0, (uint8_t *) &opt0);
+
+    twritef("%d %d\n", optoff, sizeof(*dhcp_disc));
+
     struct sockaddr_in addr;
-    addr.sin_port = htons(20);
+    addr.sin_port = htons(DHCP_DEST_PORT);
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = BROADCAST_IP;
-
-    char* message = "Hello world!";
-    int ret = sendto(dhcp_socket, message, strlen(message), 0, (struct sockaddr*) &addr, 0);
+    int ret = sendto(dhcp_socket, dhcp_disc, sizeof(*dhcp_disc)+optoff, 0, (struct sockaddr*) &addr, 0);
 
 
 
