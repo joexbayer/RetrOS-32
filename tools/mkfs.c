35805d47d9ee65e594dc60feff3c24a12d5779b1
Joe Bayer <jb71999@gmail.com>
2022-08-11 22:40:10 +0200

    MFKS: Comments

--- tools/mkfs.c
+++ tools/mkfs.c
@@ -1,3 +1,15 @@
+/**
+ * @file mkfs.c
+ * @author Joe Bayer (joexbayer)
+ * @brief "Normal" C program, creating a NETOS filesystem and adds programs / files.
+ * Compiled to 32 bit as it is linked with the ENTOS fs/ objects and headers. 
+ * @version 0.1
+ * @date 2022-08-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -35,10 +47,17 @@ int get_current_time(struct time* time_s){
     time_s->second = tm.tm_sec;
 }
 
+/* Functions needed for inode and bitmap to work. */
 int32_t dbgprintf(char* fmt, ...)
 {
+    return 0;
 }
 
+void* alloc(int size){
+    return malloc(size);
+}
+
+/* Functions simulating the disk device read / write functions. */
 int read_block(char* buf, int block)
 {
     fseek(filesystem, block*512, SEEK_SET);
@@ -48,7 +67,6 @@ int read_block(char* buf, int block)
 
 int write_block(char* buf, int block)
 {
-    printf("Write: 0x%x. Block %d\n", buf, block*512);
     fseek(filesystem, block*512, SEEK_SET);
     fwrite(buf, 1, 512, filesystem);
     return 1;
@@ -72,10 +90,7 @@ int read_block_offset(char* usr_buf, int size, int offset, int block)
     return size;   
 }
 
-void* alloc(int size){
-    return malloc(size);
-}
-
+/* Helper function used by fs */
 static inline void __inode_add_dir(struct directory_entry* entry, struct inode* inode, struct superblock* sb)
 {
     inode->pos = inode->size;
@@ -91,29 +106,23 @@ int main(int argc, char* argv[])
     struct superblock superblock;
 
     superblock.magic = MAGIC;
-    superblock.size = 200000;
+    superblock.size = 200000; // TODO: turn into argv parameter.
 
     superblock.ninodes = (superblock.size / (sizeof(struct inode)+NDIRECT*BLOCK_SIZE));
     superblock.nblocks = superblock.ninodes*NDIRECT;
 
+    /* This will be recaculated at runtime in the kernel based on the kernel size. */
     superblock.inodes_start = FS_START_LOCATION + 3;
     superblock.blocks_start = superblock.inodes_start + (superblock.ninodes/ INODES_PER_BLOCK);
-
-    printf("[FS]: Creating Filesystem with size: %d (%d total)\n", superblock.nblocks*BLOCK_SIZE, superblock.size);
-    printf("[FS]: With a total of %d inodes (%d blocks)\n", superblock.ninodes, superblock.ninodes / INODES_PER_BLOCK);
-    printf("[FS]: And total of %d block\n", superblock.nblocks);
-    printf("[FS]: Max file size: %d bytes\n", NDIRECT*BLOCK_SIZE);
 
     superblock.block_map = create_bitmap(superblock.nblocks);
     superblock.inode_map = create_bitmap(superblock.ninodes);
 
     inode_t root_inode = alloc_inode(&superblock, FS_DIRECTORY);
     struct inode* root = inode_get(root_inode, &superblock);
-
     superblock.root_inode = root_inode;
 
-    printf("[FS]: Root node: %d\n", root_inode);
-
+    /* Basic directories */
     struct directory_entry self = {
         .inode = root_inode,
         .name = "."
@@ -146,6 +155,12 @@ int main(int argc, char* argv[])
     write_block_offset((char*) &superblock, sizeof(struct superblock), 0, FS_START_LOCATION);
     write_block_offset((char*) superblock.inode_map, get_bitmap_size(superblock.ninodes), 0, FS_INODE_BMAP_LOCATION);
     write_block_offset((char*) superblock.block_map, get_bitmap_size(superblock.nblocks), 0, FS_BLOCK_BMAP_LOCATION);
+
+    printf("[FS]: Creating Filesystem with size: %d (%d total)\n", superblock.nblocks*BLOCK_SIZE, superblock.size);
+    printf("[FS]: With a total of %d inodes (%d blocks)\n", superblock.ninodes, superblock.ninodes / INODES_PER_BLOCK);
+    printf("[FS]: And total of %d block\n", superblock.nblocks);
+    printf("[FS]: Max file size: %d bytes\n", NDIRECT*BLOCK_SIZE);
+    printf("[FS] Written and saved filesystem to filesystem.image!\n");
 
     fclose(filesystem);
 
