a14520ceb71943c25c49c84f3dae12bb95687780
Joe Bayer <jb71999@gmail.com>
2022-08-28 10:04:22 +0200

    TCP: Functions

---
+++ net/tcp.c
@@ -0,0 +1,18 @@
+#include <net/tcp.h>
+
+#define MAX_TCP_CONNECTIONS 20
+
+static tcp_connection tcp_connections[20];
+
+int tcp_register_connection(uint16_t dst_port, uint16_t src_port)
+{
+
+    return 0;
+}
+
+void tcp_connection_update()
+{
+
+    return;
+}
+
