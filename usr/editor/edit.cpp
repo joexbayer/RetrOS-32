576877240fc93ab8a1b34a501926c421d907a415
Joe Bayer <jb71999@gmail.com>
2023-04-20 10:58:00 +0200

    Fix: editor now works again

--- usr/editor/edit.cpp
+++ usr/editor/edit.cpp
@@ -8,17 +8,16 @@ public:
 	Editor() : Window(288, 248, "Editor") {
 		m_x = 0;
 		m_y = 0;
-		textBuffer = (unsigned char*) malloc((c_width/8)*(c_height/8));
+		m_textBuffer = (unsigned char*) malloc((c_width/8)*(c_height/8));
 		gfx_draw_rectangle(0, 0, 288, 248, COLOR_BOX_LIGHT_FG);
-		gfx_draw_line(0, 17, 248, 17, 0xff);
+		gfx_draw_line(17, 0, 17, 248, 0xff);
 		for (int i = 0; i < 248; i++){
 			gfx_draw_format_text(0, i*8, 0xff, "%s%d ", i < 10 ? " " : "", i);
 		}
-		
 	}
 
 	~Editor() {
-		free(textBuffer);
+		free(m_textBuffer);
 		close(m_fd);
 	}
 
@@ -29,13 +28,30 @@ public:
 
 private:
 	int m_fd = -1;
-	unsigned char* textBuffer;
+	unsigned char* m_textBuffer;
+	int m_bufferSize = 0;
 	int m_x, m_y;
 
 	/* Size is based on the fact that our filesystem can only handle 8kb files */
 	const int c_width = 280;
 	const int c_height = 240;
+
+	void reDraw();
 };
+
+void Editor::reDraw()
+{	
+	m_x = 0;
+	m_y = 0;
+	gfx_draw_rectangle(0, 0, 288, 248, COLOR_BOX_LIGHT_FG);
+	gfx_draw_line(17, 0, 17, 248, 0xff);
+	for (int i = 0; i < 248; i++){
+		gfx_draw_format_text(0, i*8, 0xff, "%s%d ", i < 10 ? " " : "", i);
+	}
+	for (int i = 0; i < m_bufferSize; i++){
+		putChar(m_textBuffer[i]);
+	}
+}
 
 void Editor::Open(char* path)
 {
@@ -43,10 +59,8 @@ void Editor::Open(char* path)
 	if(m_fd <= 0)
 		return;
 	
-	int ret = read(m_fd, textBuffer, (c_width/8)*(c_height/8));
-	for (int i = 0; i < ret; i++){
-		putChar(textBuffer[i]);
-	}
+	m_bufferSize = read(m_fd, m_textBuffer, (c_width/8)*(c_height/8));
+	reDraw();
 	
 }
 
@@ -58,7 +72,10 @@ void Editor::EditorLoop()
 		gfx_get_event(&event);
 		switch (event.event){
 		case GFX_EVENT_KEYBOARD:
+			m_bufferSize++;
 			putChar(event.data);
+			if(event.data == '\n')
+				reDraw();
 			break;
 		default:
 			break;
@@ -67,26 +84,26 @@ void Editor::EditorLoop()
 }
 
 void Editor::putChar(unsigned char c)
-{
-	textBuffer[m_y*(c_width/8) + m_x] = c;
+{	
+	if(c != '\b')
+		m_textBuffer[m_y*(c_width/8) + m_x] = c;
 	gfx_draw_rectangle(24 + m_x*8, m_y*8, 8, 8, COLOR_BOX_LIGHT_FG);
 
 	switch (c){
 	case '\n':
-		m_x = 0;
-		m_y++;
-		gfx_draw_char(24 + m_x*8, m_y*8, '_', COLOR_BOX_BG);
-		return;
+		m_x = (c_width/8)+1;
+		break;;
 	case '\b':
 		if(m_x == 0) return;
 		m_x--;
-		gfx_draw_rectangle(24 + m_x*8, m_y*8, 8, 8, COLOR_BOX_LIGHT_FG);
-		gfx_draw_char(24 + m_x*8, m_y*8, '_', COLOR_BOX_BG);
+		m_bufferSize--;
+		//gfx_draw_rectangle(24 + m_x*8, m_y*8, 8, 8, COLOR_BOX_LIGHT_FG);
+		//gfx_draw_char(24 + m_x*8, m_y*8, '_', COLOR_BOX_BG);
 		return;
 	case 254:
 		if(m_x == 0 || m_y == 0) return;
 		gfx_draw_rectangle(24 + m_x*8, m_y*8, 8, 8, COLOR_BOX_LIGHT_FG);
-		gfx_draw_char(24 + m_x*8, m_y*8, textBuffer[m_y*(c_width/8) + m_x], COLOR_BOX_LIGHT_FG);
+		gfx_draw_char(24 + m_x*8, m_y*8, m_textBuffer[m_y*(c_width/8) + m_x], COLOR_BOX_LIGHT_FG);
 
 		m_y--;
 		gfx_draw_char(24 + m_x*8, m_y*8, '_', COLOR_BOX_BG);
@@ -105,11 +122,11 @@ void Editor::putChar(unsigned char c)
 	gfx_draw_char(24 + m_x*8, m_y*8, '_', COLOR_BOX_BG);
 }
 
-int main(int argc, char** argv)
+int main()
 {
-	for (int i = 0; i < argc; i++){
-		printf("argv: %s\n", argv[i]);
-	}
+	//for (int i = 0; i < argc; i++){
+	//	printf("argv: %s\n", argv[i]);
+	//}
 	
 
     Editor s1;
