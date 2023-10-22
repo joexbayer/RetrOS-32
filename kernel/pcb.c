26db012b2a56a642b6d13f7b4130965b96904792
Joe Bayer <jb71999@gmail.com>
2023-10-22 18:57:08 +0200

    pcb: thread creation and process code cleanup

--- kernel/pcb.c
+++ kernel/pcb.c
@@ -316,6 +316,27 @@ void idletask(){
 	};
 }
 
+static struct pcb* pcb_get_free()
+{
+	int i; /* Find a pcb is that is "free" */
+	for(i = 0; i < MAX_NUM_OF_PCBS; i++)
+		if(pcb_table[i].state == STOPPED){
+			pcb_table[i].pid = i;
+			return &pcb_table[i];
+		}
+
+	return NULL;
+}
+
+static void pcb_free(struct pcb* pcb)
+{
+	memset(pcb, 0, sizeof(struct pcb));
+	pcb->state = STOPPED;
+	pcb->pid = -1;
+	pcb->next = NULL;
+	pcb->prev = NULL;
+}
+
 
 void dummytask(){
 	int j = 0;
@@ -432,8 +453,11 @@ error_t pcb_init_kthread(int pid, struct pcb* pcb, void (*entry)(), char* name)
 	/* this is done for processes in vmem.c, should probably be moved there? */
 	pcb->allocations = kalloc(sizeof(struct virtual_allocations));
 	if(pcb->allocations == NULL){
-		kernel_panic("Out of memory while allocating virtual memory allocations.");
+		pcb_free(pcb);
+		dbgprintf("[PCB] Failed to allocate memory for virtual allocations\n");
+		return -ERROR_ALLOC;
 	}
+
 	pcb->allocations->head = NULL;
 	pcb->allocations->spinlock = 0;
 
@@ -446,41 +470,121 @@ void flush_tlb() {
     asm volatile ("movl %0, %%cr3" : : "r" (cr3));
 }
 
+/**
+ * @brief Creates a new kernel thread.
+ * Creates a process thread which inherets the parents virtual memory.
+ * @param entry entry function
+ * @param name name of thread
+ * @param flags flags for thread
+ * @return int pid of thread, -1 on error.
+ */
+error_t pcb_create_thread(struct pcb* parent, void (*entry)(), char* name, byte_t flags)
+{
+	ENTER_CRITICAL();
+
+	struct pcb* pcb = pcb_get_free();
+	if(pcb == NULL){
+		dbgprintf("No free PCBs!\n");
+		return -ERROR_PCB_FULL;
+	}
+
+	pcb->ctx.eip = (uint32_t)entry;
+	pcb->ctx.esp = 0xEFFFFFF0;
+	pcb->ctx.ebp = pcb->ctx.esp;
+
+	/* Stack grows down so we want the upper part of allocated memory.*/
+	pcb->stackptr = (uint32_t) kalloc(PCB_STACK_SIZE);
+	if((void*)pcb->stackptr == NULL){
+		dbgprintf("[PCB] STACK == NULL");
+		pcb_free(pcb);
+		return -ERROR_ALLOC;
+	}
+	memset((void*)pcb->stackptr, 0, PCB_STACK_SIZE);
+	pcb->kesp = pcb->stackptr+PCB_STACK_SIZE-1;
+	pcb->kebp = pcb->kesp;
+
+	/* a process thread still is treated as a process */
+	pcb->is_process = 1;
+
+	pcb->term = parent->term;
+	pcb->kallocs = 0;
+	pcb->preempts = 0;
+	pcb->args = 0;
+	pcb->argv = NULL;
+	pcb->yields = 0;
+	pcb->parent = current_running;
+	pcb->cs = GDT_PROCESS_CS | PROCESSS_PRIVILEGE;
+    pcb->ds = GDT_PROCESS_DS | PROCESSS_PRIVILEGE;
+
+	/* Launch with kernel privileges */
+	if(flags & PCB_FLAG_KERNEL){
+		pcb->cs = GDT_KERNEL_CS;
+		pcb->ds = GDT_KERNEL_DS;
+	}
+
+	/* inheret parents virtual memory */
+	vmem_init_process_thread(parent, pcb);
+
+	get_scheduler()->ops->add(get_scheduler(), pcb);
+
+	pcb_count++;
+	LEAVE_CRITICAL();
+	pcb->state = PCB_NEW;
+	return pcb->pid;
+}
+
 error_t pcb_create_process(char* program, int argc, char** argv, pcb_flag_t flags)
 {
 	ENTER_CRITICAL();
 	/* Load process from disk */
 	inode_t inode = fs_open(program, FS_FILE_FLAG_READ);
-	if(inode < 0) return -ERROR_FILE_NOT_FOUND;
+	if(inode < 0){
+		return -ERROR_FILE_NOT_FOUND;
+	}
 
-	dbgprintf("[INIT PROCESS] Reading %s from disk\n", program);
+	dbgprintf("Reading %s from disk\n", program);
 	char* buf = kalloc(MAX_FILE_SIZE);
+	if(buf == NULL){
+		dbgprintf("Error allocating memory for %s\n", program);
+		fs_close(inode);
+		return -ERROR_ALLOC;
+	}
+
+	/* Read file into buffer */
 	int read = fs_read(inode, buf, MAX_FILE_SIZE);
-	if(read < 0)
-		return -1;
+	if(read < 0){
+		kfree(buf);
+		fs_close(inode);
+		return -ERROR_FILE_NOT_FOUND;
+	}
 
 	fs_close(inode);
 
 	/* Create stack and pcb */
-	int i; /* Find a pcb is that is "free" */
-	for(i = 0; i < MAX_NUM_OF_PCBS; i++)
-		if(pcb_table[i].state == STOPPED)
-			break;
-		
-	assert(pcb_table[i].state == STOPPED);
-	
-	struct pcb* pcb = &pcb_table[i];
+	struct pcb* pcb = pcb_get_free();
+	if(pcb == NULL){
+		kfree(buf);
+		fs_close(inode);
+		return -ERROR_PCB_FULL;
+	}
 
 	pcb->ctx.eip = 0x1000000; /* External programs start */
-	pcb->pid = i;
 	pcb->data_size = read;
 	memcpy(pcb->name, program, strlen(program)+1);
 	pcb->ctx.esp = 0xEFFFFFF0;
 	pcb->ctx.ebp = pcb->ctx.esp;
+	
 	pcb->stackptr = (uint32_t) kalloc(PCB_STACK_SIZE);
+	if((void*)pcb->stackptr == NULL){
+		kfree(buf);
+		fs_close(inode);
+		pcb_free(pcb);
+		return -ERROR_ALLOC;
+	}
 	memset((void*)pcb->stackptr, 0, PCB_STACK_SIZE);
+	
+	dbgprintf("Setup PCB %d for %s\n", pcb->pid, program);
 	pcb->kesp = pcb->stackptr+PCB_STACK_SIZE-1;
-	dbgprintf("[INIT PROCESS] Setup PCB %d for %s\n", i, program);
 	pcb->kebp = pcb->kesp;
 	pcb->term = current_running->term;
 	pcb->is_process = 1;
@@ -523,16 +627,16 @@ error_t pcb_create_process(char* program, int argc, char** argv, pcb_flag_t flag
 	}
 
 
-	get_scheduler()->ops->add(get_scheduler(), &pcb_table[i]);
-	//running->ops->add(running, pcb);
+	get_scheduler()->ops->add(get_scheduler(), pcb);
+	// TODO: Check for errors
 
 	pcb_count++;
 	LEAVE_CRITICAL();
 	kfree(buf);
-	dbgprintf("[INIT PROCESS] Created new process!\n");
+	dbgprintf("Created new process!\n");
 	pcb->state = PCB_NEW;
 	/* Run */
-	return i;
+	return pcb->pid;
 }
 
 /**
@@ -546,33 +650,27 @@ error_t pcb_create_process(char* program, int argc, char** argv, pcb_flag_t flag
 error_t pcb_create_kthread(void (*entry)(), char* name)
 {   
 	ENTER_CRITICAL();
-	if(MAX_NUM_OF_PCBS == pcb_count){
-		dbgprintf("All PCBs are in use!\n");
+	
+	struct pcb* pcb = pcb_get_free();
+	if(pcb == NULL){
+		dbgprintf("No free PCBs!\n");
 		return -ERROR_PCB_FULL;
 	}
-
-	int i; /* Find a pcb is that is "free" */
-	for(i = 0; i < MAX_NUM_OF_PCBS; i++){
-		if(pcb_table[i].state == STOPPED){
-			break;
-		}
-	}
 	
-	int ret = pcb_init_kthread(i, &pcb_table[i], entry, name);
+	int ret = pcb_init_kthread(pcb->pid, pcb, entry, name);
 	assert(ret);
 
-	get_scheduler()->ops->add(get_scheduler(), &pcb_table[i]);
+	get_scheduler()->ops->add(get_scheduler(), pcb);
 	//running->ops->push(running, &pcb_table[i]);
 
 	pcb_count++;
-	dbgprintf("Added %s, PID: %d, Stack: 0x%x\n", name, i, pcb_table[i].kesp);
+	dbgprintf("Added %s, PID: %d, Stack: 0x%x\n", name, pcb->pid, pcb->kesp);
 	LEAVE_CRITICAL();
-	return i;
+	return pcb->pid;
 }
 
 void __noreturn start_pcb(struct pcb* pcb)
 {   
-
 	pcb->state = RUNNING;
 	dbgprintf("[START PCB] Starting pcb!\n");
 	//pcb_dbg_print(current_running);
