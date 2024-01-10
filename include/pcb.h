3809bc6e16a46123f1b6f8d380f8ae3579c4b240
Joe Bayer <jb71999@gmail.com>
2024-01-10 20:53:28 +0100

    re add removed macro

--- include/pcb.h
+++ include/pcb.h
@@ -15,6 +15,16 @@ struct pcb;
 
 #define PCB_STACK_SIZE 0x2000
 
+#define AS_THREAD(block) \
+do { \
+    void* __kthread_internal(void* _unused) { \
+        block \
+        return NULL; \
+    } \
+    pcb_create_kthread(_kthread_func, "kthread", 0, NULL); \
+} while (0)
+
+
 typedef int pid_t;
 
 /* TODO: Move to new file */
@@ -107,7 +117,7 @@ struct process {
 
 extern const char* pcb_status[];
 extern struct pcb* $current_process;
-extern struct process* $process;
+extern struct proceses* $process;
 
 /* Forward declaration */
 struct pcb_queue;
