f3c88437b5e2c05bcab1ee6d6f1f7abecd10310d
Joe Bayer <jb71999@gmail.com>
2024-02-03 20:06:55 +0100

    progress on editor

--- kernel/edit.c
+++ kernel/edit.c
@@ -8,32 +8,46 @@
 #include <keyboard.h>
 
 #define MAX_LINES 512
-
-struct line {
-    char *text;
-    size_t length;
-    size_t capacity;
-};
+#define LINE_CAPACITY 78
 
 struct textbuffer {
-    struct line **lines;
-    size_t line_count;
+    struct line {
+        char *text;
+        size_t length;
+        size_t capacity;
+    } **lines;
     struct cursor {
         size_t x;
         size_t y;
     } cursor;
+    size_t line_count;
 };
+static int textbuffer_new_line(struct textbuffer *buffer);
 
 static struct textbuffer *textbuffer_create(void)
 {
+    int ret;
     struct textbuffer *buffer = create(struct textbuffer);
+    if(buffer == NULL){
+        return NULL;
+    }
+    
     buffer->lines = kalloc(sizeof(struct line*) * MAX_LINES);
-    buffer->lines[0]->text = kalloc(32);
-    buffer->line_count = 1;
-    buffer->lines[0]->length = 0;
-    buffer->lines[0]->capacity = 32;
+    if(buffer->lines == NULL){
+        kfree(buffer);
+        return NULL;
+    }
+
+    ret = textbuffer_new_line(buffer);
+    if(ret < 0){
+        kfree(buffer->lines);
+        kfree(buffer);
+        return NULL;
+    }
+
     buffer->cursor.x = 0;
     buffer->cursor.y = 0;
+
     return buffer;
 }
 
@@ -41,12 +55,12 @@ static void textbuffer_destroy(struct textbuffer *buffer)
 {
     for (size_t i = 0; i < buffer->line_count; i++) {
         if (buffer->lines[i]->text) {
-            free(buffer->lines[i]->text);
+            kfree(buffer->lines[i]->text);
         }
-        free(buffer->lines[i]);
+        kfree(buffer->lines[i]);
     }
-    free(buffer->lines);
-    free(buffer);
+    kfree(buffer->lines);
+    kfree(buffer);
 }
 
 static int textbuffer_new_line(struct textbuffer *buffer)
@@ -56,9 +70,20 @@ static int textbuffer_new_line(struct textbuffer *buffer)
     }
 
     buffer->lines[buffer->line_count] = kalloc(sizeof(struct line));
-    buffer->lines[buffer->line_count]->text = kalloc(32);
+    if(buffer->lines[buffer->line_count] == NULL){
+        return -1;
+    }
+
+    buffer->lines[buffer->line_count]->text = kalloc(LINE_CAPACITY);
+    if(buffer->lines[buffer->line_count]->text == NULL){
+        kfree(buffer->lines[buffer->line_count]);
+        return -1;
+    }
+
+    memset(buffer->lines[buffer->line_count]->text, 0, LINE_CAPACITY);
+    
     buffer->lines[buffer->line_count]->length = 0;
-    buffer->lines[buffer->line_count]->capacity = 32;
+    buffer->lines[buffer->line_count]->capacity = LINE_CAPACITY;
     buffer->line_count++;
 
     return 0;
@@ -71,9 +96,28 @@ static int textbuffer_remove_last_line(struct textbuffer *buffer)
     }
 
     kfree(buffer->lines[buffer->line_count - 1]->text);
-    buffer->lines[buffer->line_count - 1]->text = NULL;
-    buffer->lines[buffer->line_count - 1]->length = 0;
-    buffer->lines[buffer->line_count - 1]->capacity = 0;
+    kfree(buffer->lines[buffer->line_count - 1]);
+    buffer->line_count--;
+
+    return 0;
+}
+
+static int textbuffer_free_line(struct textbuffer *buffer, size_t line)
+{
+    if (line >= buffer->line_count) {
+        return -1;
+    }
+    
+    /* Move the content of all lines below current up one line */
+    for (size_t i = line; i < buffer->line_count - 1; i++) {
+        memset(buffer->lines[i]->text, 0, buffer->lines[i]->capacity);
+        memcpy(buffer->lines[i]->text, buffer->lines[i + 1]->text, buffer->lines[i + 1]->length);
+        buffer->lines[i]->length = buffer->lines[i + 1]->length;
+    }
+
+    /* Remove the last line */
+    kfree(buffer->lines[buffer->line_count - 1]->text);
+    kfree(buffer->lines[buffer->line_count - 1]);
     buffer->line_count--;
 
     return 0;
@@ -95,7 +139,7 @@ static void textbuffer_display(const struct textbuffer *buffer, enum vga_color f
 
         /* Write the line to the screen */
         if (buffer->lines[i]->text) {
-            scrprintf(x, y, "%s", buffer->lines[i]->text);
+            scrprintf(x, 1+y, "%d: %s (l: %d)", i, buffer->lines[i]->text, buffer->lines[i]->length);
         }
     }
 
@@ -104,13 +148,15 @@ static void textbuffer_display(const struct textbuffer *buffer, enum vga_color f
         screen_set_cursor(x_start, y_start + buffer->line_count - 1);
     }
 
-    screen_set_cursor(buffer->cursor.x == 0 ? buffer->cursor.x : buffer->cursor.x -1, buffer->cursor.y);
+    screen_set_cursor(buffer->cursor.x-1+3, 1+buffer->cursor.y);
     /* write stats at the bottom */
     scrprintf(0, 24, "line_count: %d, x: %d, y: %d", buffer->line_count, buffer->cursor.x, buffer->cursor.y);
 }
 
 /* Function to handle keyboard input of a char */
 static void textbuffer_handle_char(struct textbuffer *buffer, unsigned char c) {
+    int ret;
+    
     /* Handle the backspace key */
     if (c == '\b') {
         /* If we are at the end of the line */
@@ -124,27 +170,19 @@ static void textbuffer_handle_char(struct textbuffer *buffer, unsigned char c) {
             if(buffer->lines[buffer->cursor.y]->length > 0 && buffer->lines[buffer->cursor.y - 1]->capacity - buffer->lines[buffer->cursor.y - 1]->length > buffer->lines[buffer->cursor.y]->length){
                 memcpy(buffer->lines[buffer->cursor.y - 1]->text + buffer->lines[buffer->cursor.y - 1]->length, buffer->lines[buffer->cursor.y]->text, buffer->lines[buffer->cursor.y]->length);
             }
+            buffer->cursor.x = buffer->lines[buffer->cursor.y - 1]->length;
+
             /* update new length */
             buffer->lines[buffer->cursor.y - 1]->length += buffer->lines[buffer->cursor.y]->length;
             
-            /* Move the content of all lines below current up one line */
-            if(buffer->cursor.y < buffer->line_count - 1){
-                for (size_t i = buffer->cursor.y; i < buffer->line_count - 1; i++) {
-                    buffer->lines[i]->text = buffer->lines[i + 1]->text;
-                    buffer->lines[i]->length = buffer->lines[i + 1]->length;
-                    buffer->lines[i]->capacity = buffer->lines[i + 1]->capacity;
-                }
-            }
+            textbuffer_free_line(buffer, buffer->cursor.y);
 
-            /* Remove the last line */
-            textbuffer_remove_last_line(buffer);
-
-            buffer->cursor.x = buffer->lines[buffer->cursor.y - 1]->length;
             buffer->cursor.y--;
         } else {
             /* Remove the character before the cursor */
             if(buffer->lines[buffer->cursor.y] == NULL) {
                 buffer->cursor.x = 0;
+                dbgprintf("buffer->lines[buffer->cursor.y] == NULL %d, 0x%x\n", buffer->cursor.y, buffer->lines[buffer->cursor.y]);
                 return;
             }
 
@@ -160,7 +198,7 @@ static void textbuffer_handle_char(struct textbuffer *buffer, unsigned char c) {
             }
 
             /* If x is at length, then we just remove the last character */
-            buffer->lines[buffer->cursor.y]->text[buffer->cursor.x - 1] = '\0';
+            buffer->lines[buffer->cursor.y]->text[buffer->cursor.x - 1] = 0;
             buffer->lines[buffer->cursor.y]->length--;
             buffer->cursor.x--;
         }
@@ -172,20 +210,40 @@ static void textbuffer_handle_char(struct textbuffer *buffer, unsigned char c) {
                 return;
             }
 
-            textbuffer_new_line(buffer);
+            ret = textbuffer_new_line(buffer);
+            if(ret < 0){
+                warningf("textbuffer_new_line failed\n");
+                return;
+            }
+
+            if(buffer->cursor.y != buffer->line_count - 1){
+                /* Move down the content of all lines below current */
+                for (size_t i = buffer->line_count - 1; i > buffer->cursor.y + 1; i--) {
+                    memset(buffer->lines[i]->text, 0, buffer->lines[i]->capacity);
+                    memcpy(buffer->lines[i]->text, buffer->lines[i - 1]->text, buffer->lines[i - 1]->length);
+                    buffer->lines[i]->length = buffer->lines[i - 1]->length;
+                }
+                /* clear next line */
+                memset(buffer->lines[buffer->cursor.y + 1]->text, 0, buffer->lines[buffer->cursor.y + 1]->capacity);
+                buffer->lines[buffer->cursor.y + 1]->length = 0;
+            }
 
             /* Move the cursor to the beginning of the new line */
             buffer->cursor.x = 0;
             buffer->cursor.y++;
         } else {
-            /* Move down the content of all lines below current */
-            for (size_t i = buffer->line_count - 1; i > buffer->cursor.y + 1; i--) {
-                buffer->lines[i]->text = buffer->lines[i - 1]->text;
-                buffer->lines[i]->length = buffer->lines[i - 1]->length;
-                buffer->lines[i]->capacity = buffer->lines[i - 1]->capacity;
+            /* Create a new line */
+            ret = textbuffer_new_line(buffer);
+            if(ret < 0){
+                warningf("textbuffer_new_line failed\n");
+                return;
             }
 
-            textbuffer_new_line(buffer);
+            /* Move down the content of all lines below current */
+            for (size_t i = buffer->line_count - 1; i > buffer->cursor.y + 1; i--) {
+                memcpy(buffer->lines[i]->text, buffer->lines[i - 1]->text, buffer->lines[i - 1]->length);
+                buffer->lines[i]->length = buffer->lines[i - 1]->length;
+            }
 
             /* clear next line */
             memset(buffer->lines[buffer->cursor.y + 1]->text, 0, buffer->lines[buffer->cursor.y + 1]->capacity);
@@ -222,15 +280,20 @@ static void textbuffer_handle_char(struct textbuffer *buffer, unsigned char c) {
             case ARROW_UP:
                 if(buffer->cursor.y > 0){
                     buffer->cursor.y--;
+                    if(buffer->cursor.x > buffer->lines[buffer->cursor.y]->length){
+                        buffer->cursor.x = buffer->lines[buffer->cursor.y]->length;
+                    }
                 }
                 break;
             case ARROW_DOWN:
                 if(buffer->cursor.y < buffer->line_count - 1){
                     buffer->cursor.y++;
+                    if(buffer->cursor.x > buffer->lines[buffer->cursor.y]->length){
+                        buffer->cursor.x = buffer->lines[buffer->cursor.y]->length;
+                    }
                 }
                 break;
         }
-        textbuffer_display(buffer, VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
     } else {
         /* If the buffer is full, do nothing */
         if (buffer->lines[buffer->cursor.y]->length == buffer->lines[buffer->cursor.y]->capacity) {
@@ -255,16 +318,23 @@ static void textbuffer_handle_char(struct textbuffer *buffer, unsigned char c) {
 
 static __kthread_entry void editor()
 {
-    char c;
+    unsigned char c;
     struct textbuffer *buffer = textbuffer_create();
-
+    if(buffer == NULL){
+        dbgprintf("buffer == NULL\n");
+    }
+    
+    textbuffer_display(buffer, VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
     while (1){
 		c = kb_get_char();
 		if(c == 0) continue;
+        if(c == CTRLC) break;
+
         textbuffer_handle_char(buffer, c);
         textbuffer_display(buffer, VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
 	}
 
+    textbuffer_destroy(buffer);
 }
 EXPORT_KSYMBOL(editor);
 
